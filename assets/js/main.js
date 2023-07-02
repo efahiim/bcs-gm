2cc19de10ac3640486c22ef527766e80eab8bead
Fahiim Emambocus <efahiim@gmail.com>
2023-07-02 23:55:12 +0400

    Finished responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,14 +40,14 @@ $(document).ready(function(){
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                 }
             },
