1705d9cfc1df33df5adf20368be1d78e27113da9
Fahiim Emambocus <efahiim@gmail.com>
2023-07-26 05:01:16 +0400

    Fixed order issue

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Entity\OrderDetails;
+use App\Entity\Product;
 use App\Form\OrderFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
